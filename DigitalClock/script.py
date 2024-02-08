5fc4f580d55fbf681aa3f2e9a754f4c0d5a92c94
TheProjectsX <manvstech2.0@gmail.com>
2024-02-07 20:52:21 -0800

    Digital Clock Complete

--- DigitalClock/script.py
+++ DigitalClock/script.py
@@ -1,28 +1,13 @@
-import datetime
-import time
-from pyscript import document
+import asyncio
+from datetime import datetime
+from pyscript import display
 
-# Elements
-hour1 = document.getElementById("hour1")
-hour2 = document.getElementById("hour2")
-
-minute1 = document.getElementById("minute1")
-minute2 = document.getElementById("minute2")
+# Update Time
 
 
-second1 = document.getElementById("second1")
-second2 = document.getElementById("second2")
-
-ampm = document.getElementById("ampm")
-
-date = document.getElementById("date")
-
-
-
-# Update time
 def updateTime():
     # Get the current date and time
-    now = datetime.datetime.now()
+    now = datetime.now()
 
     # Extract Time
     hour_now = now.strftime("%H")   # HH
@@ -30,29 +15,34 @@ def updateTime():
     second_now = now.strftime("%S")  # SS
     ampm_now = now.strftime("%p")   # AM/PM
 
-    date_now = now.strftime("%a, %b %d, %Y") # Date
-
+    date_now = now.strftime("%a, %b %d, %Y")  # Date
 
     # Setting Hour
-    hour1.innerText = hour_now[0]
-    hour2.innerText = hour_now[1]
+    display(hour_now[0], target="hour1", append=False)
+    display(hour_now[1], target="hour2", append=False)
 
     # Setting Minutes
-    minute1.innerText = minute_now[0]
-    minute2.innerText = minute_now[1]
+    display(minute_now[0], target="minute1", append=False)
+    display(minute_now[1], target="minute2", append=False)
 
     # Setting Seconds
-    second1.innerText = second_now[0]
-    second2.innerText = second_now[0]
+    display(second_now[0], target="second1", append=False)
+    display(second_now[1], target="second2", append=False)
 
     # Setting AM/PM
-    ampm.innerText = ampm_now
+    display(ampm_now, target="ampm", append=False)
 
     # Setting Date
-    date.innerText = date_now
+    display(date_now, target="date", append=False)
 
 
 # Start the Loop!
-while True:
-    updateTime()
-    time.sleep(1)
+async def init():
+    while True:
+        updateTime()
+        await asyncio.sleep(1)
+
+
+# Initialize the Clock
+# This below line may normally show Error (Can't use await outside async function). But in Py-Script, it will work. As the script tag has an "async" attribute
+await init()
